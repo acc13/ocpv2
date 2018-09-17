f4246f9fce41b24fed485a87b7f1495c33e4b42c
Andrew Chang <andrew_chang1@hotmail.com>
2018-09-17 14:10:30 -0400

    fixing lambda entry points, added env prefix to ApiGateway REST API

--- deploy/testDeployLambda.sh
+++ deploy/testDeployLambda.sh
@@ -3,13 +3,16 @@
 DEPLOY_FOLDER="${0%/*}"
 BUCKET=deployocp
 INVITE_S3_KEY=lambas/InviteCandidate-1.0.zip
+ENV_PREFIX=test_
 
 
 #copy web content first
 LAMBDAS=$DEPLOY_FOLDER/../lambdas
 ZIP=$LAMBDAS/InviteCandidate/build/distributions/InviteCandidate-1.0.zip
 aws s3 cp $ZIP s3://$BUCKET/$INVITE_S3_KEY
-aws lambda update-function-code --function-name Ocpv2InviteCandidate --s3-bucket $BUCKET --s3-key $INVITE_S3_KEY
+aws lambda update-function-code --function-name ${ENV_PREFIX}InviteCandidate --s3-bucket $BUCKET --s3-key $INVITE_S3_KEY
+aws lambda update-function-code --function-name ${ENV_PREFIX}TestOutput --s3-bucket $BUCKET --s3-key $INVITE_S3_KEY
+aws lambda update-function-code --function-name ${ENV_PREFIX}NotifyCodeUploaded --s3-bucket $BUCKET --s3-key $INVITE_S3_KEY
 
 echo
 echo Deployment ended
--- web/js/config.js
+++ web/js/config.js
@@ -4,7 +4,7 @@ var isProd = window.location.href.indexOf("test.yetanotherwhatever.io") == -1;
 var isTest = !isProd;
 
 var stage = isProd? "prod": "test";
-var ocpv2RestAPIId = "87n9iujcs3";
+var ocpv2RestAPIId = "wbwgw94qf2";
 
 var restAPIBaseURL = "https://" + ocpv2RestAPIId + ".execute-api.us-east-1.amazonaws.com/" + stage;
 
