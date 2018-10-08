105c225c8159e560b1a91e193c9662b5677d37d1
acc13 <andrew_chang1@hotmail.com>
2018-10-08 16:14:33 -0400

    internship registration page added.

--- web/js/config.js
+++ web/js/config.js
@@ -29,7 +29,7 @@ const env = extractStackName(window.location.href);
  ****************** ENV SPECIFIC VARS  ****************** 
  */
 
-//TODO fix this
+//TODO fix this so it pulls from cloudformation stack output
 let ocpv2RestAPIId = "rz34guof5h";
 
 switch(env) {
@@ -42,53 +42,88 @@ switch(env) {
 }
 
 
-/*************************** AUTO-GENERATED FORM SETUP CODE ***************************/
+/************************** AUTO-GENERATED FORM SETUP JS **************************/
 function prepareS3UploadForms() {
-    switch(env) {
-        case 'test':
+  switch(env) {
+    case 'test':
 
+      if ( $( "#outputForm" ).length )  //if element exists
+      {
+        $('#outputForm').attr('action', 'http://test.upload.yetanotherwhatever.io.s3.amazonaws.com/');
+        $('#outputForm input[name=success_action_redirect]').val('http://test.yetanotherwhatever.io/submitting.html');
+      
+        $('#outputForm input[name=x-amz-credential]').val('AKIAJEEOFNEEWPGAZM5A/20191008/us-east-1/s3/aws4_request');
+        $('#outputForm input[name=x-amz-date]').val('20191008T000000Z');
+      
+        $('#outputForm input[name=policy]').val('eydleHBpcmF0aW9uJzonMjAxOS0xMC0wOFQwMDowMDowMFonLCdjb25kaXRpb25zJzpbeydidWNrZXQnOid0ZXN0LnVwbG9hZC55ZXRhbm90aGVyd2hhdGV2ZXIuaW8nfSxbJ3N0YXJ0cy13aXRoJywnJGtleScsJ3VwbG9hZHMvb3V0cHV0LyddLHsnYWNsJzoncHJpdmF0ZSd9LHsnc3VjY2Vzc19hY3Rpb25fcmVkaXJlY3QnOidodHRwOi8vdGVzdC55ZXRhbm90aGVyd2hhdGV2ZXIuaW8vc3VibWl0dGluZy5odG1sJ30seyd4LWFtei1hbGdvcml0aG0nOidBV1M0LUhNQUMtU0hBMjU2J30seyd4LWFtei1jcmVkZW50aWFsJzonQUtJQUpFRU9GTkVFV1BHQVpNNUEvMjAxOTEwMDgvdXMtZWFzdC0xL3MzL2F3czRfcmVxdWVzdCd9LHsneC1hbXotZGF0ZSc6JzIwMTkxMDA4VDAwMDAwMFonfSx7J3gtYW16LXN0b3JhZ2UtY2xhc3MnOidSRURVQ0VEX1JFRFVOREFOQ1knfSxbJ3N0YXJ0cy13aXRoJywnJHgtYW16LW1ldGEtZGF0YScsJyddLFsnY29udGVudC1sZW5ndGgtcmFuZ2UnLDAsMTA0ODU3Nl1dfQ==');
+        $('#outputForm input[name=x-amz-signature]').val('365e68b1170dd216ddeb0dafe9f52fc9d330938d93ebeadce5ef24d699ec7dec');
+      }
 
-              $('#outputForm').attr('action', 'http://test.upload.yetanotherwhatever.io.s3.amazonaws.com/');
-              $('#outputForm input[name=success_action_redirect]').val('http://test.yetanotherwhatever.io/submitting.html');
-            
-              $('#outputForm input[name=x-amz-credential]').val('AKIAIHTQVUJUR2IWOCOA/20191001/us-east-1/s3/aws4_request');
-              $('#outputForm input[name=x-amz-date]').val('20191001T000000Z');
-            
-              $('#outputForm input[name=policy]').val('eydleHBpcmF0aW9uJzonMjAxOS0xMC0wMVQwMDowMDowMFonLCdjb25kaXRpb25zJzpbeydidWNrZXQnOid0ZXN0LnVwbG9hZC55ZXRhbm90aGVyd2hhdGV2ZXIuaW8nfSxbJ3N0YXJ0cy13aXRoJywnJGtleScsJ3VwbG9hZHMvb3V0cHV0LyddLHsnYWNsJzoncHJpdmF0ZSd9LHsnc3VjY2Vzc19hY3Rpb25fcmVkaXJlY3QnOidodHRwOi8vdGVzdC55ZXRhbm90aGVyd2hhdGV2ZXIuaW8vc3VibWl0dGluZy5odG1sJ30seyd4LWFtei1hbGdvcml0aG0nOidBV1M0LUhNQUMtU0hBMjU2J30seyd4LWFtei1jcmVkZW50aWFsJzonQUtJQUlIVFFWVUpVUjJJV09DT0EvMjAxOTEwMDEvdXMtZWFzdC0xL3MzL2F3czRfcmVxdWVzdCd9LHsneC1hbXotZGF0ZSc6JzIwMTkxMDAxVDAwMDAwMFonfSx7J3gtYW16LXN0b3JhZ2UtY2xhc3MnOidSRURVQ0VEX1JFRFVOREFOQ1knfSxbJ3N0YXJ0cy13aXRoJywnJHgtYW16LW1ldGEtZGF0YScsJyddLFsnY29udGVudC1sZW5ndGgtcmFuZ2UnLDAsMTA0ODU3Nl1dfQ==');
-              $('#outputForm input[name=x-amz-signature]').val('dd5300923b50e223492e6f9a91c82eca9bc8bf26babd61405e14f05fa45562dd');
+      if ( $( "#codeForm" ).length )  //if element exists
+      {
+        $('#codeForm').attr('action', 'http://test.upload.yetanotherwhatever.io.s3.amazonaws.com/');
+        $('#codeForm input[name=success_action_redirect]').val('http://test.yetanotherwhatever.io/thanks.html');
+      
+        $('#codeForm input[name=x-amz-credential]').val('AKIAJEEOFNEEWPGAZM5A/20191008/us-east-1/s3/aws4_request');
+        $('#codeForm input[name=x-amz-date]').val('20191008T000000Z');
+      
+        $('#codeForm input[name=policy]').val('eydleHBpcmF0aW9uJzonMjAxOS0xMC0wOFQwMDowMDowMFonLCdjb25kaXRpb25zJzpbeydidWNrZXQnOid0ZXN0LnVwbG9hZC55ZXRhbm90aGVyd2hhdGV2ZXIuaW8nfSxbJ3N0YXJ0cy13aXRoJywnJGtleScsJ3VwbG9hZHMvY29kZS8nXSx7J2FjbCc6J3ByaXZhdGUnfSx7J3N1Y2Nlc3NfYWN0aW9uX3JlZGlyZWN0JzonaHR0cDovL3Rlc3QueWV0YW5vdGhlcndoYXRldmVyLmlvL3RoYW5rcy5odG1sJ30seyd4LWFtei1hbGdvcml0aG0nOidBV1M0LUhNQUMtU0hBMjU2J30seyd4LWFtei1jcmVkZW50aWFsJzonQUtJQUpFRU9GTkVFV1BHQVpNNUEvMjAxOTEwMDgvdXMtZWFzdC0xL3MzL2F3czRfcmVxdWVzdCd9LHsneC1hbXotZGF0ZSc6JzIwMTkxMDA4VDAwMDAwMFonfSx7J3gtYW16LXN0b3JhZ2UtY2xhc3MnOidSRURVQ0VEX1JFRFVOREFOQ1knfSxbJ3N0YXJ0cy13aXRoJywnJHgtYW16LW1ldGEtZGF0YScsJyddLFsnY29udGVudC1sZW5ndGgtcmFuZ2UnLDAsMTA0ODU3Nl1dfQ==');
+        $('#codeForm input[name=x-amz-signature]').val('30928468b7e6f5faceef1ce42d7a98b4f1e583321cbace006b5a9ba81028c479');
+      }
 
-              $('#codeForm').attr('action', 'http://test.upload.yetanotherwhatever.io.s3.amazonaws.com/');
-              $('#codeForm input[name=success_action_redirect]').val('http://test.yetanotherwhatever.io/thanks.html');
-            
-              $('#codeForm input[name=x-amz-credential]').val('AKIAIHTQVUJUR2IWOCOA/20191001/us-east-1/s3/aws4_request');
-              $('#codeForm input[name=x-amz-date]').val('20191001T000000Z');
-            
-              $('#codeForm input[name=policy]').val('eydleHBpcmF0aW9uJzonMjAxOS0xMC0wMVQwMDowMDowMFonLCdjb25kaXRpb25zJzpbeydidWNrZXQnOid0ZXN0LnVwbG9hZC55ZXRhbm90aGVyd2hhdGV2ZXIuaW8nfSxbJ3N0YXJ0cy13aXRoJywnJGtleScsJ3VwbG9hZHMvY29kZS8nXSx7J2FjbCc6J3ByaXZhdGUnfSx7J3N1Y2Nlc3NfYWN0aW9uX3JlZGlyZWN0JzonaHR0cDovL3Rlc3QueWV0YW5vdGhlcndoYXRldmVyLmlvL3RoYW5rcy5odG1sJ30seyd4LWFtei1hbGdvcml0aG0nOidBV1M0LUhNQUMtU0hBMjU2J30seyd4LWFtei1jcmVkZW50aWFsJzonQUtJQUlIVFFWVUpVUjJJV09DT0EvMjAxOTEwMDEvdXMtZWFzdC0xL3MzL2F3czRfcmVxdWVzdCd9LHsneC1hbXotZGF0ZSc6JzIwMTkxMDAxVDAwMDAwMFonfSx7J3gtYW16LXN0b3JhZ2UtY2xhc3MnOidSRURVQ0VEX1JFRFVOREFOQ1knfSxbJ3N0YXJ0cy13aXRoJywnJHgtYW16LW1ldGEtZGF0YScsJyddLFsnY29udGVudC1sZW5ndGgtcmFuZ2UnLDAsMTA0ODU3Nl1dfQ==');
-              $('#codeForm input[name=x-amz-signature]').val('99d4f6970a7c86dde15000a252bcad554721a51fcd6016ccc90f9aa7a5220bac');
-            break;
-        case 'ocp':
+      if ( $( "#internRegForm" ).length ) //if element exists
+      {
+        $('#internRegForm').attr('action', 'http://test.upload.yetanotherwhatever.io.s3.amazonaws.com/');
+        $('#internRegForm input[name=success_action_redirect]').val('http://test.yetanotherwhatever.io/registrationComplete.html');
+      
+        $('#internRegForm input[name=x-amz-credential]').val('AKIAJEEOFNEEWPGAZM5A/20191008/us-east-1/s3/aws4_request');
+        $('#internRegForm input[name=x-amz-date]').val('20191008T000000Z');
+      
+        $('#internRegForm input[name=policy]').val('eydleHBpcmF0aW9uJzonMjAxOS0xMC0wOFQwMDowMDowMFonLCdjb25kaXRpb25zJzpbeydidWNrZXQnOid0ZXN0LnVwbG9hZC55ZXRhbm90aGVyd2hhdGV2ZXIuaW8nfSxbJ3N0YXJ0cy13aXRoJywnJGtleScsJ3VwbG9hZHMvaW50ZXJuc2hpcFJlZ2lzdHJhdGlvbi8nXSx7J2FjbCc6J3ByaXZhdGUnfSx7J3N1Y2Nlc3NfYWN0aW9uX3JlZGlyZWN0JzonaHR0cDovL3Rlc3QueWV0YW5vdGhlcndoYXRldmVyLmlvL3JlZ2lzdHJhdGlvbkNvbXBsZXRlLmh0bWwnfSx7J3gtYW16LWFsZ29yaXRobSc6J0FXUzQtSE1BQy1TSEEyNTYnfSx7J3gtYW16LWNyZWRlbnRpYWwnOidBS0lBSkVFT0ZORUVXUEdBWk01QS8yMDE5MTAwOC91cy1lYXN0LTEvczMvYXdzNF9yZXF1ZXN0J30seyd4LWFtei1kYXRlJzonMjAxOTEwMDhUMDAwMDAwWid9LHsneC1hbXotc3RvcmFnZS1jbGFzcyc6J1JFRFVDRURfUkVEVU5EQU5DWSd9LFsnc3RhcnRzLXdpdGgnLCckeC1hbXotbWV0YS1kYXRhJywnJ10sWydjb250ZW50LWxlbmd0aC1yYW5nZScsMCwxMDQ4NTc2XV19');
+        $('#internRegForm input[name=x-amz-signature]').val('35cbaa77f43b1a55290ceaf62608dbbeb743b33be5cdd6d4aef35fcfa8dc6a79');
+      }
+      break;
+    case 'ocp':
 
-              $('#outputForm').attr('action', 'http://ocp.upload.yetanotherwhatever.io.s3.amazonaws.com/');
-              $('#outputForm input[name=success_action_redirect]').val('http://ocp.yetanotherwhatever.io/submitting.html');
-            
-              $('#outputForm input[name=x-amz-credential]').val('AKIAIHTQVUJUR2IWOCOA/20191001/us-east-1/s3/aws4_request');
-              $('#outputForm input[name=x-amz-date]').val('20191001T000000Z');
-            
-              $('#outputForm input[name=policy]').val('eydleHBpcmF0aW9uJzonMjAxOS0xMC0wMVQwMDowMDowMFonLCdjb25kaXRpb25zJzpbeydidWNrZXQnOidvY3AudXBsb2FkLnlldGFub3RoZXJ3aGF0ZXZlci5pbyd9LFsnc3RhcnRzLXdpdGgnLCcka2V5JywndXBsb2Fkcy9vdXRwdXQvJ10seydhY2wnOidwcml2YXRlJ30seydzdWNjZXNzX2FjdGlvbl9yZWRpcmVjdCc6J2h0dHA6Ly9vY3AueWV0YW5vdGhlcndoYXRldmVyLmlvL3N1Ym1pdHRpbmcuaHRtbCd9LHsneC1hbXotYWxnb3JpdGhtJzonQVdTNC1ITUFDLVNIQTI1Nid9LHsneC1hbXotY3JlZGVudGlhbCc6J0FLSUFJSFRRVlVKVVIySVdPQ09BLzIwMTkxMDAxL3VzLWVhc3QtMS9zMy9hd3M0X3JlcXVlc3QnfSx7J3gtYW16LWRhdGUnOicyMDE5MTAwMVQwMDAwMDBaJ30seyd4LWFtei1zdG9yYWdlLWNsYXNzJzonUkVEVUNFRF9SRURVTkRBTkNZJ30sWydzdGFydHMtd2l0aCcsJyR4LWFtei1tZXRhLWRhdGEnLCcnXSxbJ2NvbnRlbnQtbGVuZ3RoLXJhbmdlJywwLDEwNDg1NzZdXX0=');
-              $('#outputForm input[name=x-amz-signature]').val('39f20ba2558d96174c9a9143526ab40f16720c268c88be01cb8752dc3ea2f1e9');
+      if ( $( "#outputForm" ).length )  //if element exists
+      {
+        $('#outputForm').attr('action', 'http://ocp.upload.yetanotherwhatever.io.s3.amazonaws.com/');
+        $('#outputForm input[name=success_action_redirect]').val('http://ocp.yetanotherwhatever.io/submitting.html');
+      
+        $('#outputForm input[name=x-amz-credential]').val('AKIAJEEOFNEEWPGAZM5A/20191008/us-east-1/s3/aws4_request');
+        $('#outputForm input[name=x-amz-date]').val('20191008T000000Z');
+      
+        $('#outputForm input[name=policy]').val('eydleHBpcmF0aW9uJzonMjAxOS0xMC0wOFQwMDowMDowMFonLCdjb25kaXRpb25zJzpbeydidWNrZXQnOidvY3AudXBsb2FkLnlldGFub3RoZXJ3aGF0ZXZlci5pbyd9LFsnc3RhcnRzLXdpdGgnLCcka2V5JywndXBsb2Fkcy9vdXRwdXQvJ10seydhY2wnOidwcml2YXRlJ30seydzdWNjZXNzX2FjdGlvbl9yZWRpcmVjdCc6J2h0dHA6Ly9vY3AueWV0YW5vdGhlcndoYXRldmVyLmlvL3N1Ym1pdHRpbmcuaHRtbCd9LHsneC1hbXotYWxnb3JpdGhtJzonQVdTNC1ITUFDLVNIQTI1Nid9LHsneC1hbXotY3JlZGVudGlhbCc6J0FLSUFKRUVPRk5FRVdQR0FaTTVBLzIwMTkxMDA4L3VzLWVhc3QtMS9zMy9hd3M0X3JlcXVlc3QnfSx7J3gtYW16LWRhdGUnOicyMDE5MTAwOFQwMDAwMDBaJ30seyd4LWFtei1zdG9yYWdlLWNsYXNzJzonUkVEVUNFRF9SRURVTkRBTkNZJ30sWydzdGFydHMtd2l0aCcsJyR4LWFtei1tZXRhLWRhdGEnLCcnXSxbJ2NvbnRlbnQtbGVuZ3RoLXJhbmdlJywwLDEwNDg1NzZdXX0=');
+        $('#outputForm input[name=x-amz-signature]').val('652fe542823e6ad8bff2c7e652623c72e6bbfa6f16a9d1a867f3849b3c7ae055');
+      }
 
-              $('#codeForm').attr('action', 'http://ocp.upload.yetanotherwhatever.io.s3.amazonaws.com/');
-              $('#codeForm input[name=success_action_redirect]').val('http://ocp.yetanotherwhatever.io/thanks.html');
-            
-              $('#codeForm input[name=x-amz-credential]').val('AKIAIHTQVUJUR2IWOCOA/20191001/us-east-1/s3/aws4_request');
-              $('#codeForm input[name=x-amz-date]').val('20191001T000000Z');
-            
-              $('#codeForm input[name=policy]').val('eydleHBpcmF0aW9uJzonMjAxOS0xMC0wMVQwMDowMDowMFonLCdjb25kaXRpb25zJzpbeydidWNrZXQnOidvY3AudXBsb2FkLnlldGFub3RoZXJ3aGF0ZXZlci5pbyd9LFsnc3RhcnRzLXdpdGgnLCcka2V5JywndXBsb2Fkcy9jb2RlLyddLHsnYWNsJzoncHJpdmF0ZSd9LHsnc3VjY2Vzc19hY3Rpb25fcmVkaXJlY3QnOidodHRwOi8vb2NwLnlldGFub3RoZXJ3aGF0ZXZlci5pby90aGFua3MuaHRtbCd9LHsneC1hbXotYWxnb3JpdGhtJzonQVdTNC1ITUFDLVNIQTI1Nid9LHsneC1hbXotY3JlZGVudGlhbCc6J0FLSUFJSFRRVlVKVVIySVdPQ09BLzIwMTkxMDAxL3VzLWVhc3QtMS9zMy9hd3M0X3JlcXVlc3QnfSx7J3gtYW16LWRhdGUnOicyMDE5MTAwMVQwMDAwMDBaJ30seyd4LWFtei1zdG9yYWdlLWNsYXNzJzonUkVEVUNFRF9SRURVTkRBTkNZJ30sWydzdGFydHMtd2l0aCcsJyR4LWFtei1tZXRhLWRhdGEnLCcnXSxbJ2NvbnRlbnQtbGVuZ3RoLXJhbmdlJywwLDEwNDg1NzZdXX0=');
-              $('#codeForm input[name=x-amz-signature]').val('15fa3a3103ab652331c004bdea28ed717b549ea58eb39c107946c49a64bbaef6');
-            break;
-    }
+      if ( $( "#codeForm" ).length )  //if element exists
+      {
+        $('#codeForm').attr('action', 'http://ocp.upload.yetanotherwhatever.io.s3.amazonaws.com/');
+        $('#codeForm input[name=success_action_redirect]').val('http://ocp.yetanotherwhatever.io/thanks.html');
+      
+        $('#codeForm input[name=x-amz-credential]').val('AKIAJEEOFNEEWPGAZM5A/20191008/us-east-1/s3/aws4_request');
+        $('#codeForm input[name=x-amz-date]').val('20191008T000000Z');
+      
+        $('#codeForm input[name=policy]').val('eydleHBpcmF0aW9uJzonMjAxOS0xMC0wOFQwMDowMDowMFonLCdjb25kaXRpb25zJzpbeydidWNrZXQnOidvY3AudXBsb2FkLnlldGFub3RoZXJ3aGF0ZXZlci5pbyd9LFsnc3RhcnRzLXdpdGgnLCcka2V5JywndXBsb2Fkcy9jb2RlLyddLHsnYWNsJzoncHJpdmF0ZSd9LHsnc3VjY2Vzc19hY3Rpb25fcmVkaXJlY3QnOidodHRwOi8vb2NwLnlldGFub3RoZXJ3aGF0ZXZlci5pby90aGFua3MuaHRtbCd9LHsneC1hbXotYWxnb3JpdGhtJzonQVdTNC1ITUFDLVNIQTI1Nid9LHsneC1hbXotY3JlZGVudGlhbCc6J0FLSUFKRUVPRk5FRVdQR0FaTTVBLzIwMTkxMDA4L3VzLWVhc3QtMS9zMy9hd3M0X3JlcXVlc3QnfSx7J3gtYW16LWRhdGUnOicyMDE5MTAwOFQwMDAwMDBaJ30seyd4LWFtei1zdG9yYWdlLWNsYXNzJzonUkVEVUNFRF9SRURVTkRBTkNZJ30sWydzdGFydHMtd2l0aCcsJyR4LWFtei1tZXRhLWRhdGEnLCcnXSxbJ2NvbnRlbnQtbGVuZ3RoLXJhbmdlJywwLDEwNDg1NzZdXX0=');
+        $('#codeForm input[name=x-amz-signature]').val('4aec724992daa66402348dc6429cdc5dba1efb9821f2a91ff15a3c7f7e8d9478');
+      }
+
+      if ( $( "#internRegForm" ).length ) //if element exists
+      {
+        $('#internRegForm').attr('action', 'http://ocp.upload.yetanotherwhatever.io.s3.amazonaws.com/');
+        $('#internRegForm input[name=success_action_redirect]').val('http://ocp.yetanotherwhatever.io/registrationComplete.html');
+      
+        $('#internRegForm input[name=x-amz-credential]').val('AKIAJEEOFNEEWPGAZM5A/20191008/us-east-1/s3/aws4_request');
+        $('#internRegForm input[name=x-amz-date]').val('20191008T000000Z');
+      
+        $('#internRegForm input[name=policy]').val('eydleHBpcmF0aW9uJzonMjAxOS0xMC0wOFQwMDowMDowMFonLCdjb25kaXRpb25zJzpbeydidWNrZXQnOidvY3AudXBsb2FkLnlldGFub3RoZXJ3aGF0ZXZlci5pbyd9LFsnc3RhcnRzLXdpdGgnLCcka2V5JywndXBsb2Fkcy9pbnRlcm5zaGlwUmVnaXN0cmF0aW9uLyddLHsnYWNsJzoncHJpdmF0ZSd9LHsnc3VjY2Vzc19hY3Rpb25fcmVkaXJlY3QnOidodHRwOi8vb2NwLnlldGFub3RoZXJ3aGF0ZXZlci5pby9yZWdpc3RyYXRpb25Db21wbGV0ZS5odG1sJ30seyd4LWFtei1hbGdvcml0aG0nOidBV1M0LUhNQUMtU0hBMjU2J30seyd4LWFtei1jcmVkZW50aWFsJzonQUtJQUpFRU9GTkVFV1BHQVpNNUEvMjAxOTEwMDgvdXMtZWFzdC0xL3MzL2F3czRfcmVxdWVzdCd9LHsneC1hbXotZGF0ZSc6JzIwMTkxMDA4VDAwMDAwMFonfSx7J3gtYW16LXN0b3JhZ2UtY2xhc3MnOidSRURVQ0VEX1JFRFVOREFOQ1knfSxbJ3N0YXJ0cy13aXRoJywnJHgtYW16LW1ldGEtZGF0YScsJyddLFsnY29udGVudC1sZW5ndGgtcmFuZ2UnLDAsMTA0ODU3Nl1dfQ==');
+        $('#internRegForm input[name=x-amz-signature]').val('8aa194f36622ee703cfa8cbd24faf238e73c840230e41be14904d1278a1dbe29');
+      }
+      break;
+  }
 }
-/*************************** END AUTO-GENERATED FORM SETUP CODE ***************************/
+/************************** END AUTO-GENERATED FORM SETUP CODE **************************/
 
 //API gateway stage name matches env name
 const stage = env;
---
+++ web/js/uploadforms.js
@@ -0,0 +1,291 @@
+'use strict';
+
+/*!
+Math.uuid.js (v1.4)
+http://www.broofa.com
+mailto:robert@broofa.com
+
+Copyright (c) 2010 Robert Kieffer
+Dual licensed under the MIT and GPL licenses.
+*/
+
+/*
+ * Generate a random uuid.
+ *
+ * USAGE: Math.uuid(length, radix)
+ *   length - the desired number of characters
+ *   radix  - the number of allowable values for each character.
+ *
+ * EXAMPLES:
+ *   // No arguments  - returns RFC4122, version 4 ID
+ *   >>> Math.uuid()
+ *   "92329D39-6F5C-4520-ABFC-AAB64544E172"
+ *
+ *   // One argument - returns ID of the specified length
+ *   >>> Math.uuid(15)     // 15 character ID (default base=62)
+ *   "VcydxgltxrVZSTV"
+ *
+ *   // Two arguments - returns ID of the specified length, and radix. (Radix must be <= 62)
+ *   >>> Math.uuid(8, 2)  // 8 character ID (base=2)
+ *   "01001010"
+ *   >>> Math.uuid(8, 10) // 8 character ID (base=10)
+ *   "47473046"
+ *   >>> Math.uuid(8, 16) // 8 character ID (base=16)
+ *   "098F4D35"
+ */
+(function() {
+  // Private array of chars to use
+  const LEGAL = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const CHARS = LEGAL.split("");
+
+  Math.uuid = function (len, radix) {
+    const chars = CHARS;
+    let uuid = [];
+      let i;
+    radix = radix || chars.length;
+
+    if (len) {
+      // Compact form
+      for (i = 0;i < len; i+=1) {
+        uuid[i] = chars[0 | Math.random()*radix];
+      }
+    } else {
+      // rfc4122, version 4 form
+      let r;
+
+      // rfc4122 requires these characters
+      uuid[8] = uuid[13] = uuid[18] = uuid[23] = "-";
+      uuid[14] = "4";
+
+      // Fill in random data.  At i===19 set the high bits of clock sequence as
+      // per rfc4122, sec. 4.1.5
+      for (i = 0;i < 36;i+=1) {
+        if (!uuid[i]) {
+          r = 0 | Math.random()*16;
+          uuid[i] = chars[(i === 19) ? (r & 0x3) | 0x8 : r];
+        }
+      }
+    }
+
+    return uuid.join("");
+  };
+
+  // A more performant, but slightly bulkier, RFC4122v4 solution.  We boost performance
+  // by minimizing calls to random()
+  Math.uuidFast = function() {
+    const chars = CHARS;
+    let uuid = new Array(36);
+    let rnd=0;
+      let r;
+    for (var i = 0;i < 36;i+=1) {
+      if (i===8 || i===13 ||  i===18 || i===23) {
+        uuid[i] = "-";
+      } else if (i===14) {
+        uuid[i] = "4";
+      } else {
+        if (rnd <= 0x02) rnd = 0x2000000 + (Math.random()*0x1000000)|0;
+        r = rnd & 0xf;
+        rnd = rnd >> 4;
+        uuid[i] = chars[(i === 19) ? (r & 0x3) | 0x8 : r];
+      }
+    }
+    return uuid.join("");
+  };
+
+  // A more compact, but less performant, RFC4122v4 solution:
+  Math.uuidCompact = function() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+      const r = Math.random()*16|0, v = c === "x" ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  };
+})();
+
+
+
+
+
+
+function getProblemName()
+{
+  //this var is set by the base page
+  return problemName;
+}
+
+function setMeta(formName, myObj)
+{
+  debug(JSON.stringify(myObj));
+
+  const form = document.getElementById(formName);
+  const meta = form.querySelectorAll("[name=x-amz-meta-data]");
+  let metaInput = meta[0];
+  metaInput.value=JSON.stringify(myObj);
+}
+
+
+//Submit test output form metadata
+function setCodeMeta(formName, problemName, email)
+{
+  const myObj = {"email" : email,
+    "inviteId" : getFilenameNoExtension(),
+    "problemName" : getProblemName()
+  };
+
+  setMeta(formName, myObj);
+}
+
+
+function setOutputMeta(formName, inviteId)
+{
+  const myObj = {
+    "inviteId" : inviteId,
+    "problemName" : getProblemName()
+  };
+
+  setMeta(formName, myObj);
+}
+
+
+//Key Generation
+/* key for solution submission */
+//TODO break this up into validation, and prepare key/metadata
+function prepareoutputForm()
+{
+  prepareS3UploadForms();
+
+  //validate
+  if(!document.getElementById("outputFile").value)
+  {
+        alert("No file selected.");
+        return false;
+  }
+
+  //build key
+  const keyVal = "uploads/output/" + getFilenameNoExtension() + "/" + getProblemName() + "/" + Math.uuid() + ".txt";
+
+  debug("keyval" + keyVal);
+
+  const formName = "outputForm";
+  const form = document.getElementById(formName);
+  let keys = form.querySelectorAll("[name=key]");
+  let keyInput = keys[0];
+  keyInput.value = keyVal;
+
+  debug(keyInput.value);
+
+  setOutputMeta(formName, getFilenameNoExtension());
+}
+
+
+//given "http://foo.bar/index.html"
+//returns "index"
+function getFilenameNoExtension()
+{
+  const path = window.location.pathname;
+  const filename = path.split("/").pop();
+  return filename.split('.').slice(0, -1).join('.');
+}
+
+
+// TODO break this function up
+// int validation and prepare key/metadata
+function preparecodeForm()
+{
+
+  prepareS3UploadForms();
+  
+  /*
+   * VALIDATION 
+   */
+  const email = getAndValidateEmail();
+  if (!email)
+  {
+    return false;
+  }
+
+  const fileName = document.getElementById("codeFile").value;
+  if(!fileName)
+  {
+    alert("No file selected.");
+    return false;
+  }
+  
+  if (-1 === fileName.indexOf(".zip"))
+  {
+    alert ("Selected file is not a .zip file.");
+    return false;
+  }
+  
+  if (!confirm("Did you include your resume in your .zip file?"))
+  {
+    return false;
+  }
+
+  //build key
+  const keyVal = "uploads/code/" + getFilenameNoExtension() + "/" + Math.uuid() + ".zip";
+
+  debug("keyval" + keyVal);
+
+  const formName = "codeForm";
+  const form = document.getElementById(formName);
+  let keys = form.querySelectorAll("[name=key]");
+  let keyInput = keys[0];
+  keyInput.value = keyVal;
+
+  debug("keyInput.value" + keyInput.value);
+
+  setCodeMeta(formName, getProblemName(), email);
+
+  return true;
+
+}
+
+
+
+
+//////HANDLING THE OPTIONAL EMAIL FIELD
+function isDynamicPage()
+{
+  //page will have "/tp/" folder in the path
+  //and will be named "<UUID>.html"
+  //TODO this is a brittle pattern...
+  //breaks if the paths change
+
+  const path = window.location.pathname;
+  const pageName = path.split("/").pop();
+
+  const example = "0C109B3C-1FC3-4EEB-AF02-D604D476FF74.html";
+  const isDyn = path.indexOf("tp/") !== -1 && pageName.length === example.length;
+
+  return (isDyn);
+}
+
+$(document).ready(function(){
+  if (isDynamicPage())
+  {
+    $("#emaildiv").hide();
+  }
+});
+
+
+function getAndValidateEmail()
+{
+
+  let email = "emailindb";
+  if (!isDynamicPage())
+  {
+    const emailInput = document.getElementById("email");
+    email = emailInput.value;
+
+    if (!email || email.indexOf("@") == -1)
+    {
+      alert("Please enter a valid email address.");
+      emailInput.className="fixThis";
+      emailInput.focus();
+      return null;
+    }
+  }
+
+  return email;
+}
+
